58c49c3aa90c94634b3e6bfbf78021b1f3616e09
Samuel Fanawopo <sfanawopo@gmail.com>
2023-01-29 12:39:58 +0000

    menu on click js

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,10 @@
+document.querySelector('.menu').addEventListener('click', () => {
+	document.querySelectorAll('.target').forEach((item) => {
+		item.classList.toggle('change');
+	})
+})
+
+
 const icons = document.querySelectorAll(".section-1-icons i");
 let i = 1;
 
