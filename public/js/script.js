4490119854c5aecf58f4cf253c9eab977d89d259
Samuel Fanawopo <sfanawopo@gmail.com>
2023-02-03 18:45:59 +0000

    working on search

--- public/js/script.js
+++ public/js/script.js
@@ -1,9 +1,18 @@
+const searchForm = document.querySelector('.search');
+const container = document.querySelector(".container");
+let searchQuery = "";
+
 document.querySelector('.menu').addEventListener('click', () => {
 	document.querySelectorAll('.target').forEach((item) => {
 		item.classList.toggle('change');
 	})
 })
 
+searchForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  searchQuery = e.target.querySelector(".search").value;
+  console.log(searchQuery);
+});
 
 const icons = document.querySelectorAll(".section-1-icons i");
 let i = 1;
