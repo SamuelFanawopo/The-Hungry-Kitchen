c745009d049e14034615b55df8614daf448fbd51
Samuel Fanawopo <sfanawopo@gmail.com>
2023-02-04 22:53:14 +0000

    connect food api + connecting results to new page

--- public/js/script.js
+++ public/js/script.js
@@ -1,18 +1,55 @@
-const searchForm = document.querySelector('.search');
+const searchForm = document.querySelector(".search-box");
+const searchResultDiv = document.querySelector(".cards-container");
 const container = document.querySelector(".container");
+const signupForm = document.querySelector(".signup-form");
 let searchQuery = "";
+const APP_ID = "cea45f1d";
+const APP_key = "6bef5531ed46bbf0c6d5a2abc08cae37";
 
-document.querySelector('.menu').addEventListener('click', () => {
-	document.querySelectorAll('.target').forEach((item) => {
-		item.classList.toggle('change');
-	})
-})
+signupForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  userEmail = e.target.querySelector(".signup-form-input").value;
+  console.log(userEmail);
+});
+
+document.querySelector(".menu").addEventListener("click", () => {
+  document.querySelectorAll(".target").forEach((item) => {
+    item.classList.toggle("change");
+  });
+});
 
 searchForm.addEventListener("submit", (e) => {
   e.preventDefault();
   searchQuery = e.target.querySelector(".search").value;
-  console.log(searchQuery);
+  window.location.href="http://programminghead.com";
+  fetchAPI();
 });
+
+async function fetchAPI() {
+  // potentially make new pages, like 1, 2 after results to load page faster, e.g to 20, then next page from 21 to 40 etc.
+  const baseURL = `https://api.edamam.com/api/recipes/v2?type=public&q=pizza&app_id=${APP_ID}&app_key=${APP_key}`;
+  const res = await fetch(baseURL);
+  const data = await res.json();
+  generateHTML(data.hits);
+  console.log(data);
+}
+
+function generateHTML(results) {
+  let generatedHTML = "";
+  results.map((results) => {
+    generatedHTML += `<div class="card">
+    <img
+      src="../assets/images/temp-meal.jpg"
+      class="card-img"
+      width="230"
+      height="230"
+    />
+    <h3 class="card-name">temp</h3>
+    <button class="card-btn">View Recipe</button>
+  </div>`;
+  });
+  searchResultDiv.innerHTML = generatedHTML;
+}
 
 const icons = document.querySelectorAll(".section-1-icons i");
 let i = 1;
