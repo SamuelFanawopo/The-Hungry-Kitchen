468f94a7f1deed11b9720a73b5cff48a2a9ce204
Samuel Fanawopo <sfanawopo@gmail.com>
2023-03-08 07:25:37 +0000

    connecting to mongodb database

--- public/config/db.js
+++ public/config/db.js
@@ -1,13 +1,18 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+require("dotenv").config();
 
-const connectDB = async () => {
-    try {
-        const conn = await mongoose.connect(process.env.MONGO_URI);
-        console.log(`MongoDB connected: ${conn.connection.host}`);
-    } catch (error) {
-        console.error(`Error: ${error.message}`);
-        process.exit(1);
-    }
-}
+mongoose.connect(process.env.MONGO_URI).then(()=>{
+    console.log("mongodb connected")
+}).catch(()=>{
+    console.log("failed to connect");
+})
 
-module.exports = connectDB;
+const emailSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+});
+
+module.exports = mongoose.model("userEmails", emailSchema);
