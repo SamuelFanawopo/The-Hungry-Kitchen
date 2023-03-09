39ffe7ecfcc583cb9fda5404beb5be8b2954b581
Samuel Fanawopo <sfanawopo@gmail.com>
2023-03-09 17:12:35 +0000

    working with nodemailer

--- public/js/server.js
+++ public/js/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const nodemailer = require("nodemailer");
 const app = express();
 const connectDB = require("../config/db");
 require("dotenv").config();
@@ -21,11 +22,43 @@ app.post("/signup", async (req, res) => {
 
     await connectDB.insertMany([data]);
     console.log("user added to the database");
+
     res.render("success");
   } catch {
     console.log("user failed to be added to the database");
     res.render("failed");
   }
+
+  try {
+    // create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+      host: "smtp.office365.com",
+      port: 587,
+      secure: false, // true for 465, false for other ports
+      auth: {
+        user: process.env.EMAIL_USERNAME, // generated ethereal user
+        pass: process.env.EMAIL_PASS, // generated ethereal password
+      },
+    });
+    // send mail with defined transport object
+    let info = await transporter.sendMail({
+      from: `"noreply@thehungrykitchen" <${process.env.EMAIL_USERNAME}>`, // sender address
+      to: `${req.body.email}`, // list of receivers
+      subject: "The Hungry Kitchen", // Subject line
+      text: "This is a confirmation message for you successfully signing up to recive news about any updates or changes to the website", // plain text body
+      html: "<b>This is a confirmation message for you successfully signing up to recive news about any updates or changes to the website</b>", // html body
+    });
+
+    console.log("Message sent: %s", info.messageId);
+    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+    // Preview only available when sending through an Ethereal account
+    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+    // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+  } catch {
+    
+  }
+
 });
 
 const PORT = process.env.PORT || 5000;
